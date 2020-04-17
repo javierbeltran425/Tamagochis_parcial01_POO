65f9061dfb68223f3aefbd0d78b1bda7d2f71f4a
javierbeltran425 <00019315@uca.edu.sv>
2020-04-17 16:07:02 -0600

    ...

--- src/com/RJBM/x00019315/Main.java
+++ src/com/RJBM/x00019315/Main.java
@@ -3,6 +3,34 @@ package com.RJBM.x00019315;
 public class Main {
 
     public static void main(String[] args) {
-	int prueba;
+	    boolean continuar = true;
+        int opción = 0;
+        System.out.println("*********************");
+        System.out.println(" Sistema de empresa");
+        System.out.println("*********************");
+
+        do {
+            System.out.print("1. Agregar empleado\n2. Despedir empleado\n 3. Ver lista de empleados\n4. Calcular sueldo\n5. Mostrar totales");
+
+            switch (opción){
+                case 1:
+                    addEmpleado();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                default: System.out.print("Opción incorrecta");
+            }
+
+        }while(continuar);
+    }
+
+    public static void addEmpleado(){
+        
     }
 }
